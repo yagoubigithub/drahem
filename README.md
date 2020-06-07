f4a16474534feae254c5108598a30708c7d22df3
yagoubigithub <yagoubi.aek.2@gmail.com>
2020-06-07 19:27:43 +0200

    first commit '

---
+++ README.md
@@ -0,0 +1,35 @@
+
+
+
+[![NPM](https://img.shields.io/npm/v/drahem.svg)](https://www.npmjs.com/package/drahem) [![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+
+# drahem
+>Permet de convertir l'argent a format nombres(chiffres) en leur équivalent en lettre (ex: 1=un). Respecte l'accord prescrit par la langue française.
+
+## Install
+
+```bash
+npm install --save drahem
+```
+
+## Usage
+
+```js
+
+import { floatToDrahem } from 'drahem';
+// OR
+const { floatToDrahem } = require("drahem");
+
+console.log(floatToDrahem( 10074580.2445 ))
+
+```
+
+output : ``` dix millions soixante-quatorze mille cinq cent quatre-vingts dinars et vingt-quatre centimes```
+
+
+
+
+## License
+
+MIT © [yagoubi abdelkader](https://github.com/yagoubigithub)
---
+++ index.js
@@ -0,0 +1,190 @@
+function Unite( nombre ){
+	var unite;
+	switch( nombre ){
+		case 0: unite = "zéro";		break;
+		case 1: unite = "un";		break;
+		case 2: unite = "deux";		break;
+		case 3: unite = "trois"; 	break;
+		case 4: unite = "quatre"; 	break;
+		case 5: unite = "cinq"; 	break;
+		case 6: unite = "six"; 		break;
+		case 7: unite = "sept"; 	break;
+		case 8: unite = "huit"; 	break;
+		case 9: unite = "neuf"; 	break;
+	}//fin switch
+	return unite;
+}//-----------------------------------------------------------------------
+
+function Dizaine( nombre ){
+	switch( nombre ){
+		case 10: dizaine = "dix"; break;
+		case 11: dizaine = "onze"; break;
+		case 12: dizaine = "douze"; break;
+		case 13: dizaine = "treize"; break;
+		case 14: dizaine = "quatorze"; break;
+		case 15: dizaine = "quinze"; break;
+		case 16: dizaine = "seize"; break;
+		case 17: dizaine = "dix-sept"; break;
+		case 18: dizaine = "dix-huit"; break;
+		case 19: dizaine = "dix-neuf"; break;
+		case 20: dizaine = "vingt"; break;
+		case 30: dizaine = "trente"; break;
+		case 40: dizaine = "quarante"; break;
+		case 50: dizaine = "cinquante"; break;
+		case 60: dizaine = "soixante"; break;
+		case 70: dizaine = "soixante-dix"; break;
+		case 80: dizaine = "quatre-vingt"; break;
+		case 90: dizaine = "quatre-vingt-dix"; break;
+	}//fin switch
+	return dizaine;
+}//-----------------------------------------------------------------------
+
+function integerToLetter( nombre ){
+	var i, j, n, quotient, reste, nb ;
+	var ch
+	var numberToLetter='';
+	//__________________________________
+	
+	if(  nombre.toString().replace( / /gi, "" ).length > 15  )	return "dépassement de capacité";
+	if(  isNaN(nombre.toString().replace( / /gi, "" ))  )		return "Nombre non valide";
+
+	nb = parseFloat(nombre.toString().replace( / /gi, "" ));
+	if(  Math.ceil(nb) != nb  )	return  "Nombre avec virgule non géré.";
+	
+	n = nb.toString().length;
+	switch( n ){
+		 case 1: numberToLetter = Unite(nb); break;
+		 case 2: if(  nb > 19  ){
+					   quotient = Math.floor(nb / 10);
+					   reste = nb % 10;
+					   if(  nb < 71 || (nb > 79 && nb < 91)  ){
+							 if(  reste == 0  ) numberToLetter = Dizaine(quotient * 10);
+							 if(  reste == 1  ) numberToLetter = Dizaine(quotient * 10) + "-et-" + Unite(reste);
+							 if(  reste > 1   ) numberToLetter = Dizaine(quotient * 10) + "-" + Unite(reste);
+					   }else numberToLetter = Dizaine((quotient - 1) * 10) + "-" + Dizaine(10 + reste);
+				 }else numberToLetter = Dizaine(nb);
+				 break;
+		 case 3: quotient = Math.floor(nb / 100);
+				 reste = nb % 100;
+				 if(  quotient == 1 && reste == 0   ) numberToLetter = "cent";
+				 if(  quotient == 1 && reste != 0   ) numberToLetter = "cent" + " " + integerToLetter(reste);
+				 if(  quotient > 1 && reste == 0    ) numberToLetter = Unite(quotient) + " cents";
+				 if(  quotient > 1 && reste != 0    ) numberToLetter = Unite(quotient) + " cent " + integerToLetter(reste);
+				 break;
+		 case 4 :  quotient = Math.floor(nb / 1000);
+					  reste = nb - quotient * 1000;
+					  if(  quotient == 1 && reste == 0   ) numberToLetter = "mille";
+					  if(  quotient == 1 && reste != 0   ) numberToLetter = "mille" + " " + integerToLetter(reste);
+					  if(  quotient > 1 && reste == 0    ) numberToLetter = integerToLetter(quotient) + " mille";
+					  if(  quotient > 1 && reste != 0    ) numberToLetter = integerToLetter(quotient) + " mille " + integerToLetter(reste);
+					  break;
+		 case 5 :  quotient = Math.floor(nb / 1000);
+					  reste = nb - quotient * 1000;
+					  if(  quotient == 1 && reste == 0   ) numberToLetter = "mille";
+					  if(  quotient == 1 && reste != 0   ) numberToLetter = "mille" + " " + integerToLetter(reste);
+					  if(  quotient > 1 && reste == 0    ) numberToLetter = integerToLetter(quotient) + " mille";
+					  if(  quotient > 1 && reste != 0    ) numberToLetter = integerToLetter(quotient) + " mille " + integerToLetter(reste);
+					  break;
+		 case 6 :  quotient = Math.floor(nb / 1000);
+					  reste = nb - quotient * 1000;
+					  if(  quotient == 1 && reste == 0   ) numberToLetter = "mille";
+					  if(  quotient == 1 && reste != 0   ) numberToLetter = "mille" + " " + integerToLetter(reste);
+					  if(  quotient > 1 && reste == 0    ) numberToLetter = integerToLetter(quotient) + " mille";
+					  if(  quotient > 1 && reste != 0    ) numberToLetter = integerToLetter(quotient) + " mille " + integerToLetter(reste);
+					  break;
+		 case 7: quotient = Math.floor(nb / 1000000);
+					  reste = nb % 1000000;
+					  if(  quotient == 1 && reste == 0  ) numberToLetter = "un million";
+					  if(  quotient == 1 && reste != 0  ) numberToLetter = "un million" + " " + integerToLetter(reste);
+					  if(  quotient > 1 && reste == 0   ) numberToLetter = integerToLetter(quotient) + " millions";
+					  if(  quotient > 1 && reste != 0   ) numberToLetter = integerToLetter(quotient) + " millions " + integerToLetter(reste);
+					  break;  
+		 case 8: quotient = Math.floor(nb / 1000000);
+					  reste = nb % 1000000;
+					  if(  quotient == 1 && reste == 0  ) numberToLetter = "un million";
+					  if(  quotient == 1 && reste != 0  ) numberToLetter = "un million" + " " + integerToLetter(reste);
+					  if(  quotient > 1 && reste == 0   ) numberToLetter = integerToLetter(quotient) + " millions";
+					  if(  quotient > 1 && reste != 0   ) numberToLetter = integerToLetter(quotient) + " millions " + integerToLetter(reste);
+					  break;  
+		 case 9: quotient = Math.floor(nb / 1000000);
+					  reste = nb % 1000000;
+					  if(  quotient == 1 && reste == 0  ) numberToLetter = "un million";
+					  if(  quotient == 1 && reste != 0  ) numberToLetter = "un million" + " " + integerToLetter(reste);
+					  if(  quotient > 1 && reste == 0   ) numberToLetter = integerToLetter(quotient) + " millions";
+					  if(  quotient > 1 && reste != 0   ) numberToLetter = integerToLetter(quotient) + " millions " + integerToLetter(reste);
+					  break;  
+		 case 10: quotient = Math.floor(nb / 1000000000);
+						reste = nb - quotient * 1000000000;
+						if(  quotient == 1 && reste == 0  ) numberToLetter = "un milliard";
+						if(  quotient == 1 && reste != 0  ) numberToLetter = "un milliard" + " " + integerToLetter(reste);
+						if(  quotient > 1 && reste == 0   ) numberToLetter = integerToLetter(quotient) + " milliards";
+						if(  quotient > 1 && reste != 0   ) numberToLetter = integerToLetter(quotient) + " milliards " + integerToLetter(reste);
+					    break;	
+		 case 11: quotient = Math.floor(nb / 1000000000);
+						reste = nb - quotient * 1000000000;
+						if(  quotient == 1 && reste == 0  ) numberToLetter = "un milliard";
+						if(  quotient == 1 && reste != 0  ) numberToLetter = "un milliard" + " " + integerToLetter(reste);
+						if(  quotient > 1 && reste == 0   ) numberToLetter = integerToLetter(quotient) + " milliards";
+						if(  quotient > 1 && reste != 0   ) numberToLetter = integerToLetter(quotient) + " milliards " + integerToLetter(reste);
+					    break;	
+		 case 12: quotient = Math.floor(nb / 1000000000);
+						reste = nb - quotient * 1000000000;
+						if(  quotient == 1 && reste == 0  ) numberToLetter = "un milliard";
+						if(  quotient == 1 && reste != 0  ) numberToLetter = "un milliard" + " " + integerToLetter(reste);
+						if(  quotient > 1 && reste == 0   ) numberToLetter = integerToLetter(quotient) + " milliards";
+						if(  quotient > 1 && reste != 0   ) numberToLetter = integerToLetter(quotient) + " milliards " + integerToLetter(reste);
+					    break;	
+		 case 13: quotient = Math.floor(nb / 1000000000000);
+						reste = nb - quotient * 1000000000000;
+						if(  quotient == 1 && reste == 0  ) numberToLetter = "un billion";
+						if(  quotient == 1 && reste != 0  ) numberToLetter = "un billion" + " " + integerToLetter(reste);
+						if(  quotient > 1 && reste == 0   ) numberToLetter = integerToLetter(quotient) + " billions";
+						if(  quotient > 1 && reste != 0   ) numberToLetter = integerToLetter(quotient) + " billions " + integerToLetter(reste);
+					    break; 	
+		 case 14: quotient = Math.floor(nb / 1000000000000);
+						reste = nb - quotient * 1000000000000;
+						if(  quotient == 1 && reste == 0  ) numberToLetter = "un billion";
+						if(  quotient == 1 && reste != 0  ) numberToLetter = "un billion" + " " + integerToLetter(reste);
+						if(  quotient > 1 && reste == 0   ) numberToLetter = integerToLetter(quotient) + " billions";
+						if(  quotient > 1 && reste != 0   ) numberToLetter = integerToLetter(quotient) + " billions " + integerToLetter(reste);
+					    break; 	
+		 case 15: quotient = Math.floor(nb / 1000000000000);
+						reste = nb - quotient * 1000000000000;
+						if(  quotient == 1 && reste == 0  ) numberToLetter = "un billion";
+						if(  quotient == 1 && reste != 0  ) numberToLetter = "un billion" + " " + integerToLetter(reste);
+						if(  quotient > 1 && reste == 0   ) numberToLetter = integerToLetter(quotient) + " billions";
+						if(  quotient > 1 && reste != 0   ) numberToLetter = integerToLetter(quotient) + " billions " + integerToLetter(reste);
+					    break; 	
+	 }//fin switch
+	 /*respect de l'accord de quatre-vingt*/
+	 if(  numberToLetter.substr(numberToLetter.length-"quatre-vingt".length,"quatre-vingt".length) == "quatre-vingt"  ) numberToLetter = numberToLetter + "s";
+	 
+	 return numberToLetter;
+}
+
+function floatToDrahem(floatNumber){
+ const devise1 = "dinars";
+ const devise2 = "centimes";
+ let theFloatNumber =Number.parseFloat(floatNumber);
+
+  theFloatNumber = theFloatNumber.toFixed(2);
+
+  const theFloat = theFloatNumber.toString().split('.')[1]
+  const theInteger = theFloatNumber.toString().split('.')[0];
+
+
+
+  return `${integerToLetter(theInteger)} ${devise1} et ${integerToLetter(theFloat)} ${devise2}`
+  
+
+
+}
+
+
+console.log(floatToDrahem(10074580.2445))
+
+exports.integerToLetter = integerToLetter;
+exports.Dizaine = Dizaine;
+exports.Unite =Unite;
+exports.floatToDrahem = floatToDrahem;
+
